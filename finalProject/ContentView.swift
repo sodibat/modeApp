eb2eb44c5e9b61dc200ba0aecf1a9f9feffa439f
Sarah <s.o.odibat@gmail.com>
2023-07-18 14:47:42 -0500

    initial commit (sarah)

--- finalProject/ContentView.swift
+++ finalProject/ContentView.swift
@@ -18,27 +18,11 @@ struct ContentView: View {
 
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+            
+        
+            
+            
+            
         }
     }
 
