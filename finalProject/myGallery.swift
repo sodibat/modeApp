f243a32dad1b9ba245aaa9efc772bac5e5887a78
Katie Vo <katievo2017@gmail.com>
2023-07-20 15:19:26 -0500

    Final commit

--- finalProject/myGallery.swift
+++ finalProject/myGallery.swift
@@ -9,22 +9,98 @@ import SwiftUI
 
 struct myGallery: View {
     var body: some View {
-        NavigationStack {
-            VStack {
-                Text("MY GALLERY")
-                    .font(.largeTitle)
-                    .fontWeight(.ultraLight)
-                    .foregroundColor(Color.purple)
+                NavigationStack {
+            ZStack{
+            Image("greenBack")
+            .resizable()
+            .edgesIgnoringSafeArea(.all)
+            VStack{
+                NavigationLink(destination: ContentView()) {
+                Image("home")
+                    }
+            Image("gallery")
+                Text("A C C E S S O R I E S ")
+                HStack {
+                    Image("flowerAccessory")
+                .resizable(resizingMode: .stretch)
+                .frame(width: 70, height: 70)
+                    Image("pearlAccessory")
+                    .resizable(resizingMode: .stretch)
+                    .frame(width: 70, height: 70)
+                Image("leafAccessory")
+                .resizable(resizingMode: .stretch)
+                    .frame(width: 70, height: 70)
+                    Image("ovalAccessory")
+                    .resizable(resizingMode: .stretch)
+                    .frame(width: 70, height: 70)
+                Image("hoopAccessory")
+                        .resizable(resizingMode: .stretch)
+                        .frame(width: 70, height: 70)
+            
+                    }
+                
+                
+                Text("T O P S")
+                HStack{
+                Image("navyShirt")
+                    .resizable(resizingMode: .stretch)
+                    .frame(width: 70, height: 70)
+                Image("pinkShirt")
+                              .resizable(resizingMode: .stretch)
+                              .frame(width: 70, height: 70)
+            Image("blackShirt")
+                              .resizable(resizingMode: .stretch)
+                              .frame(width: 70, height: 70)
+            Image("stripedShirt")
+                              .resizable(resizingMode: .stretch)
+                              .frame(width: 70, height: 70)
+            Image("whiteShirt")
+                              .resizable(resizingMode: .stretch)
+                              .frame(width: 70, height: 70)
+                          }
+                          Text("B O T T O M S")
+                          HStack{
+                Image("grayBlueBottoms")
+                              .resizable(resizingMode: .stretch)
+                              .frame(width: 70, height: 70)
+            Image("blueBottoms")
+                              .resizable(resizingMode: .stretch)
+                              .frame(width: 70, height: 70)
+            Image("beigeBottoms")
+                              .resizable(resizingMode: .stretch)
+                              .frame(width: 70, height: 70)
+            Image("ivoryBottoms")
+                              .resizable(resizingMode: .stretch)
+                              .frame(width: 70, height: 70)
+            Image("blackBottoms")
+                              .resizable(resizingMode: .stretch)
+                              .frame(width: 70, height: 70)
+                          }
+                          Text("S H O E S")
+                          HStack{
+                Image("balanceShoes")
+                              .resizable(resizingMode: .stretch)
+                              .frame(width: 70, height: 70)
+                Image("converseShoes")
+                              .resizable(resizingMode: .stretch)
+                              .frame(width: 70, height: 70)
+                Image("loaferShoes")
+                              .resizable(resizingMode: .stretch)
+                              .frame(width: 70, height: 70)
+            Image("sketcherShoes")
+                              .resizable(resizingMode: .stretch)
+                              .frame(width: 70, height: 70)
+                Image("nikeShoes")
+                              .resizable(resizingMode: .stretch)
+                              .frame(width: 70, height: 70)
+                
+                          }
+                          }
+                      }
+                    }
+                  }
                 }
-                    NavigationLink(destination: addNewItem() ) {
-                        Text("ADD NEW ITEM")
-                            .font(.title2)
-                            .foregroundColor(Color.green)
-                    
-                }
-            }
-        }
-    }
+
 
 
 struct myGallery_Previews: PreviewProvider {
@@ -32,3 +108,4 @@ struct myGallery_Previews: PreviewProvider {
         myGallery()
     }
 }
+
